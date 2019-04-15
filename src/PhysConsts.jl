34cd588b8e2ca5b4ffd9dab58b8fa891f794aec3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-15 01:40:30 +0000

    Snapshot of branch master taken on 2019-04-14T21:40:29.49-04:00 from "https://github.com/UnofficialJuliaMirror/PhysConsts.jl-c3b9f364-8978-596d-a16f-335153e3eeb6"

---
+++ src/PhysConsts.jl
@@ -0,0 +1,35 @@
+__precompile__()
+
+module PhysConsts
+
+using Compat
+export DATA
+
+macro __DATA__()
+    joinpath(dirname(@__DIR__), "data")
+end
+
+mkpath(@__DATA__)
+DATA = Dict{String, Any}()
+
+include("DataFile.jl")
+
+# General Type Hierachy
+include("Core.jl")
+include("NIST.jl")
+include("Defined.jl")
+
+# Load Data
+DATAFILES = Dict{String, DataFile}()
+DATAFILES["NIST"] = DataFile(
+    "https://nist.gov/srd/srd_data//srd121_allascii_2014.json",
+    @__DATA__,
+    "srd121_allascii_2014.json"
+)
+obtain(DATAFILES["NIST"])
+
+# Constants Alias
+include("Universal.jl")
+include("Pauli.jl")
+
+end # module
